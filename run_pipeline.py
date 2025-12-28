5d05029563bd943d626ad5319aace264aeba76d5
georfil <georfilippou@gmail.com>
2025-12-28 20:14:15 +0200

    Corrected some references

--- run_pipeline.py
+++ run_pipeline.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-from scrapers.supermarkets.ab import scrape as scrape_ab
+from scrapers.ab.ab import scrape as scrape_ab
 from processing.ab_transformer import transform as transform_ab
 
 
@@ -10,7 +10,7 @@ def main():
 
     ab_raw = scrape_ab()
     ab_df = transform_ab(ab_raw)
-    
+
     final_df = pd.concat(
         [
             ab_df,
