b1d3993b0835b3f000897e791cf0a98e4ebfa38d
georfil <georfilippou@gmail.com>
2026-01-01 19:10:28 +0200

    sleep 1 -> 2

--- scrapers/ab/ab.py
+++ scrapers/ab/ab.py
@@ -66,9 +66,8 @@ def fetch_products(category_code, page):
 def scrape():
     products = []
     categories = fetch_categories()
-    print(categories)
     for category_url in categories:
-        sleep(1)
+        sleep(2)
         category_code = category_url.split("c/")[1]
         print(category_code)
         first_page = fetch_products(category_code, 0)
